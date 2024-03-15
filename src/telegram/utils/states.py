6312d5334e53cc584bc431bb6f8123ef1600ac4c
Sasha <lxudrr@gmail.com>
2024-03-15 20:52:34 +0100

    chore: change names of the fsm.states

--- src/telegram/utils/states.py
+++ src/telegram/utils/states.py
@@ -4,6 +4,6 @@ from aiogram.fsm.state import State, StatesGroup
 class ChatStates(StatesGroup):   
     settings = State()
     language = State()
-    money_limit = State()
-    coins = State()
+    moneylimit = State()
+    listofcoins = State()
 
