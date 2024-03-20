5602eb2415f09abc80668ef60bebf7809cfd7a3c
Sasha <lxudrr@gmail.com>
2024-03-20 15:58:53 +0100

    chore: update import paths

--- src/telegram/handlers/user_commands.py
+++ src/telegram/handlers/user_commands.py
@@ -8,5 +8,7 @@ router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message) -> None:
+    print(DataBase.get_connection())
+    #db_connection.add_new_user(message.from_user.id, 0, 0, 0)
     await message.answer(f'Welcome to Crypto Bot 24/7', reply_markup=reply.main_kb)
-    
+
