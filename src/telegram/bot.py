0871d6e3be251b376cbdbab64dcb265c11962415
Sasha <lxudrr@gmail.com>
2024-03-20 15:59:51 +0100

    chore: update import paths

--- src/telegram/bot.py
+++ src/telegram/bot.py
@@ -8,7 +8,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.bot import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-from handlers import bot_messages, user_commands 
+from telegram.handlers import bot_messages, user_commands 
 
 
 async def start_bot() -> None:
@@ -24,7 +24,6 @@ async def start_bot() -> None:
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-
-if __name__ == '__main__':
+def main():
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(start_bot())
