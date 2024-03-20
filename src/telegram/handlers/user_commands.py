7c830862b2170cd8b0c3f2a5a6104670bc56b447
Sasha <lxudrr@gmail.com>
2024-03-20 21:30:36 +0100

    feat: implement function template for future

--- src/telegram/handlers/user_commands.py
+++ src/telegram/handlers/user_commands.py
@@ -4,12 +4,13 @@ from aiogram.filters import CommandStart
 
 from telegram.keyboards import reply
 from data.db import DataBase
+from telegram.utils import message_distributor
 
 router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message) -> None:
-    print(DataBase.get_connection())
-    #db_connection.add_new_user(message.from_user.id, 0, 0, 0)
+    DataBase.add_new_user(message.from_user.id, 0, 0, 0)
+    await message_distributor.send_to_all_users(message.from_user.id, "hello, dude")
     await message.answer(f'Welcome to Crypto Bot 24/7', reply_markup=reply.main_kb)
 
