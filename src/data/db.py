9fc3242cfb71368629e6741d19aa0d3f31ee4fc0
Sasha <lxudrr@gmail.com>
2024-03-20 16:23:01 +0100

    feat: add add_new_user function from 'add_data'

--- src/data/db.py
+++ src/data/db.py
@@ -2,6 +2,8 @@ from psycopg2 import connect
 from dotenv import load_dotenv
 from os import getenv
 
+from data import add_data, update_data
+
 
 class DataBase:
     __instance = None
@@ -24,3 +26,9 @@ class DataBase:
         if DataBase.__instance is not None:
             DataBase.__instance.close()
             DataBase.__instance = None
+
+    # add_data.py
+    @staticmethod
+    def add_new_user(user_id: int, days_left: int, money_limit: int, list_of_coins: int) -> None:
+        add_data.add_new_user(DataBase.__instance, user_id, days_left, money_limit, list_of_coins)
+        
