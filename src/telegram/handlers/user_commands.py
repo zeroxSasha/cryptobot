fc3fec97db2803521a3e19ef01c943825b698af2
Sasha <lxudrr@gmail.com>
2024-03-27 16:41:29 +0100

    chore: change name of the imports

--- src/telegram/handlers/user_commands.py
+++ src/telegram/handlers/user_commands.py
@@ -3,14 +3,12 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart
 
 from telegram.keyboards import reply
-from data.db import DataBase
-from telegram.utils import message_distributor
+from data.postgre_db import postgre_main
 
 router = Router()
 
 @router.message(CommandStart())
 async def start(message: Message) -> None:
-    DataBase.add_new_user(message.from_user.id, 0, 0, 0)
-    await message_distributor.send_to_all_users(message.from_user.id, "hello, dude")
+    postgre_main.DataBase.add_new_user(message.from_user.id, 0, 0, 0)
     await message.answer(f'Welcome to Crypto Bot 24/7', reply_markup=reply.main_kb)
 
