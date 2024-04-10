46f029369747cfb3bab5eb9be6e32c715d78d151
Sasha <lxudrr@gmail.com>
2024-04-10 20:16:29 +0200

    chore: change logic of the function

--- src/api/coinmarketcap.py
+++ src/api/coinmarketcap.py
@@ -19,18 +19,17 @@ headers = {
 }
 
 
-async def run_coinmarketcap():
+async def run_coinmarketcap() -> list[str]:
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(url, params=parameters) as response:
             response.raise_for_status()
             
             data = await response.json()
-            count = 0
+            coins = list()
             for i in data['data']:
-                print(i['symbol'], i['id'])
-                count += 1
-            print(count)
-
+                coins.append(i['symbol'].lower() + 'usdt')
+            
+            return coins
 
 if __name__ == '__main__':
     asyncio.run(run_coinmarketcap())
