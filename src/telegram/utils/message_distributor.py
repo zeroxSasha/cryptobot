c279a665562664e1d1149074cef03e20e511190a
Sasha <lxudrr@gmail.com>
2024-04-10 20:19:25 +0200

    chore: implement asynchronous and fix misspell

--- src/telegram/utils/message_distributor.py
+++ src/telegram/utils/message_distributor.py
@@ -10,8 +10,7 @@ async def send_to_all_users() -> None:
     tgbot = await bot.TelegramBot.get_bot()
                 
     while True:
-        await asyncio.sleep(1.5)
-        data = redis_main.RedisDB.delete_last_value()
+        data = await redis_main.RedisDB.delete_last_value()
         if data:
             users = postgre_main.DataBase.get_all_users()
             data = json.loads(data)
@@ -21,7 +20,7 @@ async def send_to_all_users() -> None:
                 list_of_coins = user[3]
 
                 if money_limit * 1000 < int(data['Total']):
-                    if data['Side'] == 'Buy':
+                    if data['Side'] == 'BUY':
                         await tgbot.send_message(id, f"🔴 #{data['Symbol']} Liquidated Long: ${data['Total']} at ${data['Average Price']}")
                     else:
                         await tgbot.send_message(id, f"🟢 #{data['Symbol']} Liquidated Short: ${data['Total']} at ${data['Average Price']}")
